e87dedfe2b7b21f981b589f57db855412bd6fb4f
froste <73338az@gmail.com>
2022-11-24 17:40:33 -0600

    botón agregado

--- src/view/Base.java
+++ src/view/Base.java
@@ -40,16 +40,16 @@ public class Base extends javax.swing.JFrame {
 
         jPanel2.setPreferredSize(new java.awt.Dimension(1280, 30));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Elioenaí\\Documents\\NetBeansProjects\\southernDental\\resources\\images\\remove.png")); // NOI18N
-        jLabel1.setText("jLabel1");
+        jLabel1.setIcon(new javax.swing.ImageIcon("/home/labingsw05/NetBeansProjects/ontologiqueApp/resources/images/remove.png")); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 1216, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(1242, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
