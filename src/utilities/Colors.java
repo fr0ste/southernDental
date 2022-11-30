e228a3a3aab5eba315a8a5fe1861df8baa5c9ad4
elioenai <elioenai@elioenai-NBLB-WAX9N>
2022-11-30 15:34:55 -0600

    Ventana Home

--- src/utilities/Colors.java
+++ src/utilities/Colors.java
@@ -8,7 +8,8 @@ import java.awt.Color;
  */
 public class Colors {
 
-    public static final Color MOUSE_ENTERED = new Color(65, 168, 224);
+    public static final Color MOUSE_ENTERED = new Color(75, 140, 172);
+    public static final Color MOUSE_EXITED = new Color(0, 0, 32);
     public static final Color DARK = new Color(23, 20, 24);
     public static final Color MAIN = new Color(0,0,0, 32);
     public static final Color LIGHT = new Color(249, 250, 251);
