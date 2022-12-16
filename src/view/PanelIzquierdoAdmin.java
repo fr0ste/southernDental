b5b3dff4fa8243f6321c2ae447865b99199a303a
Elioenai <elioenai.2001@outlook.com>
2022-12-15 22:44:22 -0600

    Documentacion método btnHomeEntered(commit12)

--- src/view/PanelIzquierdoAdmin.java
+++ src/view/PanelIzquierdoAdmin.java
@@ -34,8 +34,8 @@ public class PanelIzquierdoAdmin extends javax.swing.JPanel {
 
         jPanel1 = new javax.swing.JPanel();
         pnlHome = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        btnHome = new javax.swing.JLabel();
+        icnHome = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(0, 0, 32));
         setMaximumSize(new java.awt.Dimension(240, 700));
@@ -46,14 +46,14 @@ public class PanelIzquierdoAdmin extends javax.swing.JPanel {
 
         pnlHome.setBackground(new java.awt.Color(0, 0, 32));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel1.setText("HOME");
-        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+        btnHome.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnHome.setText("HOME");
+        btnHome.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                jLabel1MouseEntered(evt);
+                btnHomeMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                jLabel1MouseExited(evt);
+                btnHomeMouseExited(evt);
             }
         });
 
@@ -62,14 +62,14 @@ public class PanelIzquierdoAdmin extends javax.swing.JPanel {
         pnlHomeLayout.setHorizontalGroup(
             pnlHomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlHomeLayout.createSequentialGroup()
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(icnHome, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnHome, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         pnlHomeLayout.setVerticalGroup(
             pnlHomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
-            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(btnHome, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+            .addComponent(icnHome, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -91,20 +91,24 @@ public class PanelIzquierdoAdmin extends javax.swing.JPanel {
 
         add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 240, 700));
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jLabel1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseEntered
+/**
+     * 
+     * otorga y gestiona el evento de mouse entered para el botón home.
+     *
+     */
+    private void btnHomeMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnHomeMouseEntered
         pnlHome.setBackground(Colors.MOUSE_ENTERED);
 
-    }//GEN-LAST:event_jLabel1MouseEntered
+    }//GEN-LAST:event_btnHomeMouseEntered
 
-    private void jLabel1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseExited
+    private void btnHomeMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnHomeMouseExited
         pnlHome.setBackground(Colors.MOUSE_EXITED);
-    }//GEN-LAST:event_jLabel1MouseExited
+    }//GEN-LAST:event_btnHomeMouseExited
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel btnHome;
+    private javax.swing.JLabel icnHome;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel pnlHome;
     // End of variables declaration//GEN-END:variables
