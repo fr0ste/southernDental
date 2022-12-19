ca349563b1ca60bd5519f7ebd3a7db8dfaf9e830
elioenai46 <elio.antonio.654@gmail.com>
2022-12-19 15:21:38 -0600

    Agregación de Labels a PanelDerechoSignosVitales
    Commit 23-19/12/22

--- src/view/PanelDerechoSignosVitales.java
+++ src/view/PanelDerechoSignosVitales.java
@@ -23,18 +23,71 @@ public class PanelDerechoSignosVitales extends javax.swing.JPanel {
     private void initComponents() {
 
         pnlDerechoBase = new javax.swing.JPanel();
+        peso = new javax.swing.JLabel();
+        estatura = new javax.swing.JLabel();
+        temperatura = new javax.swing.JLabel();
+        fcardiaca = new javax.swing.JLabel();
+        frespiratoria = new javax.swing.JLabel();
+        presion = new javax.swing.JLabel();
+        oxigeno = new javax.swing.JLabel();
 
         pnlDerechoBase.setBackground(new java.awt.Color(255, 255, 255));
+
+        peso.setForeground(new java.awt.Color(0, 0, 0));
+        peso.setText("Peso:");
+
+        estatura.setForeground(new java.awt.Color(0, 0, 0));
+        estatura.setText("Estatura:");
+
+        temperatura.setForeground(new java.awt.Color(0, 0, 0));
+        temperatura.setText("Temperatura:");
+
+        fcardiaca.setForeground(new java.awt.Color(0, 0, 0));
+        fcardiaca.setText("Frecuencia Cardiaca:");
+
+        frespiratoria.setForeground(new java.awt.Color(0, 0, 0));
+        frespiratoria.setText("Frecuencia Respiratoria:");
+
+        presion.setForeground(new java.awt.Color(0, 0, 0));
+        presion.setText("Presión Arterial:");
+
+        oxigeno.setForeground(new java.awt.Color(0, 0, 0));
+        oxigeno.setText("Saturación Oxigeno:");
 
         javax.swing.GroupLayout pnlDerechoBaseLayout = new javax.swing.GroupLayout(pnlDerechoBase);
         pnlDerechoBase.setLayout(pnlDerechoBaseLayout);
         pnlDerechoBaseLayout.setHorizontalGroup(
             pnlDerechoBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1040, Short.MAX_VALUE)
+            .addGroup(pnlDerechoBaseLayout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addGroup(pnlDerechoBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(oxigeno)
+                    .addComponent(presion)
+                    .addComponent(frespiratoria)
+                    .addComponent(fcardiaca)
+                    .addComponent(temperatura)
+                    .addComponent(estatura)
+                    .addComponent(peso))
+                .addContainerGap(815, Short.MAX_VALUE))
         );
         pnlDerechoBaseLayout.setVerticalGroup(
             pnlDerechoBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
+            .addGroup(pnlDerechoBaseLayout.createSequentialGroup()
+                .addGap(65, 65, 65)
+                .addComponent(peso)
+                .addGap(18, 18, 18)
+                .addComponent(estatura)
+                .addGap(18, 18, 18)
+                .addComponent(temperatura)
+                .addGap(18, 18, 18)
+                .addComponent(fcardiaca)
+                .addGap(18, 18, 18)
+                .addComponent(frespiratoria)
+                .addGap(18, 18, 18)
+                .addComponent(presion)
+                .addGap(18, 18, 18)
+                .addComponent(oxigeno)
+                .addContainerGap(401, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -51,6 +104,13 @@ public class PanelDerechoSignosVitales extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel estatura;
+    private javax.swing.JLabel fcardiaca;
+    private javax.swing.JLabel frespiratoria;
+    private javax.swing.JLabel oxigeno;
+    private javax.swing.JLabel peso;
     private javax.swing.JPanel pnlDerechoBase;
+    private javax.swing.JLabel presion;
+    private javax.swing.JLabel temperatura;
     // End of variables declaration//GEN-END:variables
 }
