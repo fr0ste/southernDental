281e7e7017b4fba261e9ff39ac9a194e2487508e
elioenai46 <elio.antonio.654@gmail.com>
2022-12-20 15:17:14 -0600

    Creacion nuevo panel PnlDerechoProcedimientoActual Commit 22-20/12/22

---
+++ src/view/PnlDerechoProcedimientoActual.java
@@ -0,0 +1,50 @@
+/**
+  * Autor: Joel && Elio
+  * Fecha de creación: 20 de noviembre del 2022
+  * Fecha de modificación: 20 de diciembre del 2022
+  * Descripción: Clase procedimiento actual del paciente
+  */
+package view;
+
+public class PnlDerechoProcedimientoActual extends javax.swing.JPanel {
+
+    public PnlDerechoProcedimientoActual() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        pnlDerechoBase = new javax.swing.JPanel();
+
+        pnlDerechoBase.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout pnlDerechoBaseLayout = new javax.swing.GroupLayout(pnlDerechoBase);
+        pnlDerechoBase.setLayout(pnlDerechoBaseLayout);
+        pnlDerechoBaseLayout.setHorizontalGroup(
+            pnlDerechoBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1040, Short.MAX_VALUE)
+        );
+        pnlDerechoBaseLayout.setVerticalGroup(
+            pnlDerechoBaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 700, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlDerechoBase, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnlDerechoBase, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel pnlDerechoBase;
+    // End of variables declaration//GEN-END:variables
+}
