2d0457efe4523c2fe1c373e84a8fa2fc765b5fe1
froste <froste@froste-Aspire>
2023-01-05 08:22:55 -0600

    resources: modificado. modificacion a las tablas usuarios para logueo

--- resources/DB/southernTablas.sql
+++ resources/DB/southernTablas.sql
@@ -4,13 +4,6 @@ use southern_dental;
 
 -- ******************************************** ESQUEMAS PARA LOS USUARIOS DEL LOGUEO *******************************
 
--- Tabla usuario
-CREATE TABLE usuarios
-(id_usuario INT auto_increment PRIMARY KEY,
-nombre_usuario VARCHAR (20) NOT NULL,
-pass_usuario VARCHAR (15) NOT NULL,
-email_usuario varchar (100) not NULL
-);
 
 -- Tabla tipos de roles
 CREATE TABLE roles_tipos_usuario
@@ -19,12 +12,12 @@ id_rol INT auto_increment PRIMARY KEY,
 nombre_rol VARCHAR (50) NOT NULL
 );
 
--- Tabla intermedia ( Tabla usuarios y tabla roles_tipos_usuarios
-create table roles_usuarios
-(
-id_roles_usuarios INT auto_increment PRIMARY KEY,
-id_usuario INT NOT NULL, 
-FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario),
+-- Tabla usuario
+CREATE TABLE usuarios
+(id_usuario INT auto_increment PRIMARY KEY,
+nombre_usuario VARCHAR (20) NOT NULL,
+pass_usuario VARCHAR (15) NOT NULL,
+email_usuario varchar (100) not NULL,
 id_rol int NOT NULL,
 foreign key (id_rol) references roles_tipos_usuario(id_rol)
 );
