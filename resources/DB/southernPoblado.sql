262ff8033462f4c104671e52c35b15801219a7bb
froste <froste@froste-Aspire>
2023-01-12 08:21:49 -0600

    resources: actualizado. script de poblado

--- resources/DB/southernPoblado.sql
+++ resources/DB/southernPoblado.sql
@@ -5,14 +5,19 @@ insert into roles_tipos_usuario(nombre_rol) values ('administrador'),('profesor'
 
 -- agregamos usuarios a la base de datos
 insert into usuarios(nombre_usuario,pass_usuario,email_usuario,f_id_rol) 
-values ('Carlos','carlos123','carlos@mail.com','1'),('froste','froste123','froste@mail.com','1'),
-('juan','juan123','juan@mail.com','2'),('pepe','pepe123','pepe@gmail.com','2'),
-('ana','ana123','ana@mail.com','3');
+values ('Carlos','carlos123','carlos@mail.com','1'), ('juan','juan123','juan@mail.com','2'),
+('ana','ana123','ana@mail.com','3'),('agus','a123','a@mail.com','2');
 
 
 -- agregamos a los administradores de la aplicacion
 insert into administradores(nombre1,nombre2,apellido1,apellido2,fecha_nacimiento,sexo,fk_usuario) 
 values('Carlos','Aurelio','Alcantara','Perez',"2001-01-23",'M',1),
-('Getzemani','Alejandro','Martinez','Rodriguez',"2000-06-12",'M',2),
-('Ana','','Aguilar','Alvarado',"2000-02-12",'M',3);
+('Getzemani','Alejandro','Martinez','Rodriguez',"2000-06-12",'M',2);
 
+-- agregamos datos de los profesores
+insert into catedraticos(nombre,nombre2,apellido,apellido2,numero_trabajador,f_id_usuario)
+values('Agustin','','Perez','','9212532667',4);
+
+-- agregamos datos de alumnos
+insert into alumnos(nombre,nombre2,apellido,apellido2,semestre,grupo,matricula,telefono,f_id_catedratico_responsable,f_id_usuario)
+values('ana','','Aguilar','',7,'717','125452311','9515476231',1,3); 
